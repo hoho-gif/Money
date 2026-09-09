0bcaecc8504414cdeb506c4793da71c21c8feb6f
hisaki <hsksnsn@gmail.com>
2026-09-09 18:47:39 +0900

    modified:   script.js

--- script.js
+++ script.js
@@ -123,9 +123,6 @@ async function runFindShop(latitude, longitude) {
     try {
         const response = await fetch(GAS_URL, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
             body: JSON.stringify({
                 action: "findShop",
                 latitude: latitude,
